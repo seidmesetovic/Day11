b083e71c518786c8aea91232ee76cab317db78f5
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-19 00:22:23 +0100

    ex11 gotov

---
+++ ex11/src/index.js
@@ -0,0 +1,14 @@
+function splitArrayInGroups(arr, n) {
+    var result = [];
+    for (var i = 0; i <= n - 1; i++) {
+
+        result.push(arr.splice(0, n));
+    }
+    return result;
+}
+console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5,], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+module.exports = splitArrayInGroups;
